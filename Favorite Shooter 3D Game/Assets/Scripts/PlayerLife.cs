cb1c12545f73f990f76e5e2f9da6f3df379512a8
Kolapo Adedipe <kolapoadedipe36@gmail.com>
2023-08-30 11:32:00 +0300

    Favorite Shooter 3D Game

---
+++ Favorite Shooter 3D Game/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enermy Body"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ShooterMovement>().enabled = false;
+        Invoke(nameof(ReloadLevel), 1.3f);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
